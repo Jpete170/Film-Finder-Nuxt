0280a04913d033353adf8d2996ff3ae0fecdf22c
Jackson Peters <jpete170@gmail.com>
2022-05-10 11:17:34 +1000

    added error handling for a ''null' search (user does not enter anything into the search function)

--- pages/search.vue
+++ pages/search.vue
@@ -5,10 +5,39 @@
         </div>
         <div>
             <Search/>
+            <b-form-group label="Limit Page Items" id="radio-filter">
+                <b-container>
+                    <b-row>
+                        <b-col md="auto">
+                            <b-form-radio v-model="limit" value='5' selected>5 Items</b-form-radio> 
+                        </b-col>
+                        <b-col md="auto">
+                            <b-form-radio v-model="limit" value='10'>10 Items</b-form-radio>  
+                        </b-col>
+                        <b-col md="auto">
+                            <b-form-radio v-model="limit" value='15'>15 Items</b-form-radio>
+                        </b-col>
+                        <b-col md="auto">
+                            <b-form-radio v-model="limit" value='20'>20 Items</b-form-radio>
+                        </b-col>
+                        <b-col md="auto">
+                            <b-form-radio v-model="limit" value="10000">Show All</b-form-radio>
+                        </b-col>
+                        
+                        <b-col>
+                          <b-button @click="refresh">Refresh Results</b-button>  
+                        </b-col>
+                    </b-row>
+                </b-container>
+            </b-form-group>
+            
         </div>
         <br/>
         <div>
             <h2>{{this.limit}} Results for "{{this.$route.query.query}}" in "{{this.$route.query.column}}"</h2>
+            <div v-if="!this.results_loaded">
+                <b-spinner label="" />
+            </div>
             <div v-if="this.error == 'false'">
                 <b-row align-h="center" cols="3">
                     <div v-for="r in results" :key=r.show_id>
@@ -49,25 +78,37 @@ import { axiosGet } from '../plugins/api'
     export default{
         data(){
             return{
-                column: '',
-                query: '',
-                limit: '',
+                column: this.$route.query.column,
+                query: this.$route.query.query,
+                limit: this.$route.query.limit,
                 results: [],
                 error: 'false',
                 errorMessage: [],
+                results_loaded: false,
             }
         },
         async fetch(){
             //Fill in the different elements with information from the URL
-            this.column = this.$route.query.column;
-            this.query = this.$route.query.query;
-            this.limit = this.$route.query.limit;
+            //this.column = ;
+            //this.query = ;
+            //this.limit = ;
             //
-            try{
-                this.results = await axiosGet(`/films/search?column=${this.column}&query=${this.query}&limit=${this.limit}`).then(function(res){
-                    console.log(res)
-                    return res.data
+            
+            try {
+                if(this.$route.query.column == "null"){
+                    this.results = await axiosGet('/films').then(function(res){
+                        return res.data
                     })
+                    this.results_loaded = true;
+                } else {
+                   this.results = await axiosGet(`/films/search?column=${this.column}&query=${this.query}&limit=${this.limit}`).then(function(res){
+                    //console.log(res)
+                    
+                    return res.data
+                    }) 
+                    this.results_loaded = true;
+                }
+                
             } catch(error){
                 this.error = 'true';
                 this.errorMessage = error
